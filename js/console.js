5e5a4f8c94ae1153642f1101fb0269ad17314d3e
Александр <spinhorror@mail.ru>
2015-01-17 16:50:08 +1000

    make console work for ifmo source test

--- js/console.js
+++ js/console.js
@@ -1,34 +1,34 @@
 $("document").ready(function(){
     $("#result_table").blur(function () {
-        var table_content = $("#result_table").val();
-        console.log(table_content);
+        var page = $("#result_table").val();
+        $("#display").html(JSON.stringify(ifmo_parse(page)));
     });
-    function ajaxStart(){
+    function ajax_start(){
         $('#progress').show();
     }
 
-    function ajaxStop() {
+    function ajax_stop() {
         $('#progress').hide();
     }
 
-    function parserGo(){
-        ajaxStart();
+    function parser(){
+        ajax_start();
         $.ajax({
             url: $('#result_table_url').val(),
             crossDomain: true,
             dataType: 'jsonp',
             success: function (data) {
                 alert(data);
-                ajaxStop();
+                ajax_stop();
             },
             error: function (e, g, f) {
                 console.log(e);
-                ajaxStop();
+                ajax_stop();
             }
         });
     }
 
     $("#start").click(function () {
-        parserGo();
+        parser();
     });
 });
