3007dcd3bf178f9233e7fe90b13bc6a5772bb4fa
Александр <spinhorror@mail.ru>
2015-01-17 16:48:41 +1000

    add ifmo adapter

---
+++ js/adapters/ifmo.js
@@ -0,0 +1,39 @@
+function ifmo_parse(page) {
+    var data = [];
+    $(page).find('table[class != "wrapper"]').find("tr").each(function () {
+        var row = {}
+        var prob_num = 0;
+        $(this).find("td").each(function () {
+            var key = $(this).attr('class');
+            if (key !== undefined)
+                row[key] = $(this).html();
+            else {
+                prob_num++;
+                var attempts = $(this).find("i");
+                attempts = attempts.length == 0 ? $(this).find("b") : attempts
+                if (attempts.length > 0) {
+                    row['prob_' + prob_num + '_solved'] = $(attempts).html().indexOf("+") != -1;
+                    var time = $(attempts).find("s");
+                    if (time.length > 0) {
+                        time.find("br").remove();
+                        row['prob_' + prob_num + '_solved'] = time.html();
+                        time.remove();
+                    }
+                    var attempts_cnt = $(attempts).html();
+                    if (attempts_cnt.length == 1)
+                        attempts_cnt = 1
+                    row['prob_' + prob_num + '_attempts'] = Math.abs(parseInt(attempts_cnt)) + 1;
+                }
+                else if ($(this).html() == ".") {
+                    row['prob_' + prob_num + '_solved'] = false;
+                    row['prob_' + prob_num + '_attempts'] = 0;
+                }
+
+            }
+        });
+        if (Object.keys(row).length > 0)
+            data.push(row);
+    });
+
+    return data;
+}
