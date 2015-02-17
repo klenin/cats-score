0a763de3e92f625d5682a68491c735619ad2e4a6
Александр <spinhorror@mail.ru>
2015-02-17 20:32:58 +1000

    fix bug with late loading controller.js

--- js/app/index.js
+++ js/app/index.js
@@ -2,42 +2,42 @@
     //extendGetScriptFunction('js/app/');
     //$.getScript([
     requirejs.config({baseUrl: 'js/app/'});
-    require([
-        'controller',
-        'tests/cats_xml_data',
-        'tests/ifmo_html_data',
-        'models/version',
-        'models/entity',
-        'models/event',
-        'models/chat',
-        'models/compiler',
-        'models/contest',
-        'models/prize',
-        'models/problem',
-        'models/run',
-        'models/results_table',
-        'models/user',
-        'rules/acm',
-        'adapters/cats',
-        'adapters/ifmo',
-        'adapters/codeforces',
-        'view'
-    ], function()
-    {
-        CATS.App = new CATS.Controller();
-        CATS.App.regist_adapter(new CATS.Adapter.Cats(-1, CATS.Test.cats_xml_data));
-        CATS.App.regist_adapter(new CATS.Adapter.Ifmo(-1, CATS.Test.ifmo_html_data));
-        CATS.App.regist_adapter(new CATS.Adapter.Codeforces());
-        CATS.App.regist_rule(new CATS.Rule.Acm());
+    require(['controller'], function () {
+        require([
+            'tests/cats_xml_data',
+            'tests/ifmo_html_data',
+            'models/version',
+            'models/entity',
+            'models/event',
+            'models/chat',
+            'models/compiler',
+            'models/contest',
+            'models/prize',
+            'models/problem',
+            'models/run',
+            'models/results_table',
+            'models/user',
+            'rules/acm',
+            'adapters/cats',
+            'adapters/ifmo',
+            'adapters/codeforces',
+            'view'
+        ], function () {
+            CATS.App = new CATS.Controller();
+            CATS.App.regist_adapter(new CATS.Adapter.Cats(-1, CATS.Test.cats_xml_data));
+            CATS.App.regist_adapter(new CATS.Adapter.Ifmo(-1, CATS.Test.ifmo_html_data));
+            CATS.App.regist_adapter(new CATS.Adapter.Codeforces());
+            CATS.App.regist_rule(new CATS.Rule.Acm());
 
-        var view = new CATS.View([
-            'header_contest', 'header_contests',
-            'default/table', 'default/history', 'default/contests',
-            'ifmo/table', 'ifmo/history', 'ifmo/contests',
-            'codeforces/table', 'codeforces/history', 'codeforces/contests',
-        ]);
+            var view = new CATS.View([
+                'header_contest', 'header_contests',
+                'default/table', 'default/history', 'default/contests',
+                'ifmo/table', 'ifmo/history', 'ifmo/contests',
+                'codeforces/table', 'codeforces/history', 'codeforces/contests',
+            ]);
 
-        view.display();
+            view.display();
+        });
     });
 })(jQuery);
 
--- js/console.js
+++ js/console.js
@@ -2,32 +2,32 @@ $("document").ready(function(){
     //extendGetScriptFunction('js/app/');
     //$.getScript([
     requirejs.config({baseUrl: 'js/app/'});
-    require([
-        'controller',
-        'tests/cats_xml_data',
-        'tests/ifmo_html_data',
-        'models/version',
-        'models/entity',
-        'models/event',
-        'models/chat',
-        'models/compiler',
-        'models/contest',
-        'models/prize',
-        'models/problem',
-        'models/run',
-        'models/results_table',
-        'models/user',
-        'rules/acm',
-        'adapters/cats',
-        'adapters/ifmo',
-        'tests/spec/test'
-    ], function()
-    {
-        CATS.App = new CATS.Controller();
-        CATS.App.regist_adapter(new CATS.Adapter.Cats(CATS.Test.cats_xml_data));
-        CATS.App.regist_adapter(new CATS.Adapter.Ifmo(CATS.Test.ifmo_html_data));
-        CATS.App.regist_rule(new CATS.Rule.Acm());
+    require(['controller'], function () {
+        require([
+            'tests/cats_xml_data',
+            'tests/ifmo_html_data',
+            'models/version',
+            'models/entity',
+            'models/event',
+            'models/chat',
+            'models/compiler',
+            'models/contest',
+            'models/prize',
+            'models/problem',
+            'models/run',
+            'models/results_table',
+            'models/user',
+            'rules/acm',
+            'adapters/cats',
+            'adapters/ifmo',
+            'tests/spec/test'
+        ], function () {
+            CATS.App = new CATS.Controller();
+            CATS.App.regist_adapter(new CATS.Adapter.Cats(CATS.Test.cats_xml_data));
+            CATS.App.regist_adapter(new CATS.Adapter.Ifmo(CATS.Test.ifmo_html_data));
+            CATS.App.regist_rule(new CATS.Rule.Acm());
 
-        jasmine.getEnv().execute();
+            jasmine.getEnv().execute();
+        });
     });
 });
