da691861de2625b7d47e7d87decd544397e05966
Александр <spinhorror@mail.ru>
2015-05-22 02:35:13 +1000

    charts: add parametr filter, add aggregation filter

--- app/models/chart.js
+++ app/models/chart.js
@@ -7,17 +7,18 @@ CATS.Model.Chart = Classify(CATS.Model.Entity, {
     },
 
     add_new_series: function(params) {
-        this['add_' + params.parameter + "_series"](params);
-    },
-
-    add_run_cnt_series: function(params) {
         var tbl = CATS.App.result_tables[this.table];
         var c = CATS.App.contests[tbl.contest];
-        var runs = c.runs;
         var start_time = CATS.App.contests[tbl.contests[0]].start_time;
         var next_time = CATS.App.utils.add_time(start_time, params.period);
-        var counter = 0;
         var new_series = [];
+        this['add_' + params.parameter + "_series"](tbl, c, start_time, next_time, new_series, params);
+        this.series.push({data: new_series, xaxis: 1, yaxis: this.series.length + 1});
+    },
+
+    add_run_cnt_series: function(tbl, c, start_time, next_time, new_series, params) {
+        var runs = c.runs;
+        var targets = [];
         for(var i = 0; i < runs.length; ++i) {
             var r = CATS.App.runs[runs[i]];
             var user = CATS.App.users[r.user];
@@ -26,15 +27,64 @@ CATS.Model.Chart = Classify(CATS.Model.Entity, {
                 params.problems.indexOf(r.problem) != -1 &&
                 user.some_affiliation().match(new RegExp(params.affiliation)) &&
                 user.name.match(new RegExp(params.user))
-            ) {
-                counter++;
-            }
+            )
+                targets.push(1);
+
             if (CATS.App.utils.get_time_diff(r.start_processing_time, next_time) < 0) {
-                new_series.push([(new_series.length + 1) * params.period, counter]);
+                new_series.push([(new_series.length + 1) * params.period, this["aggregation_" + params.aggregation](targets)]);
                 next_time = CATS.App.utils.add_time(next_time, params.period);
             }
         }
-        new_series.push([(new_series.length + 1) * params.period, counter]);
-        this.series.push({data: new_series, xaxis: 1, yaxis: this.series.length + 1});
-    }
+        new_series.push([(new_series.length + 1) * params.period, this["aggregation_" + params.aggregation](targets)]);
+    },
+
+    add_points_series: function(tbl, c, start_time, next_time, new_series, params) {
+        var runs = c.runs;
+        var targets = [];
+        for(var i = 0; i < runs.length; ++i) {
+            var r = CATS.App.runs[runs[i]];
+            var user = CATS.App.users[r.user];
+            if (user.name.match(new RegExp(params.user)))
+                targets.push(r.points);
+
+            if (CATS.App.utils.get_time_diff(r.start_processing_time, next_time) < 0) {
+                new_series.push([(new_series.length + 1) * params.period, this["aggregation_" + params.aggregation](targets)]);
+                next_time = CATS.App.utils.add_time(next_time, params.period);
+            }
+        }
+        new_series.push([(new_series.length + 1) * params.period, this["aggregation_" + params.aggregation](targets)]);
+    },
+
+    add_place_series: function(tbl, c, start_time, next_time, new_series, params) {
+        var sb = tbl.score_board;
+        var targets = [];
+        for(var i = 0; i < sb.length; ++i) {
+            var r = sb[i];
+            var user = CATS.App.users[r.user];
+            if (user.some_affiliation().match(new RegExp(params.affiliation)))
+                targets.push(r.place);
+
+            if (CATS.App.utils.get_time_diff(r.start_processing_time, next_time) < 0) {
+                new_series.push([(new_series.length + 1) * params.period, this["aggregation_" + params.aggregation](targets)]);
+                next_time = CATS.App.utils.add_time(next_time, params.period);
+            }
+        }
+        new_series.push([(new_series.length + 1) * params.period, this["aggregation_" + params.aggregation](targets)]);
+    },
+
+    aggregation_sum: function(arr) {
+        return arr.reduce(function(pv, cv) { return pv + cv; }, 0);
+    },
+
+    aggregation_avg: function(arr) {
+        return arr.reduce(function(pv, cv) { return pv + cv; }, 0) / arr.length;
+    },
+
+    aggregation_min: function(arr) {
+        return Math.min.apply(null, arr);
+    },
+
+    aggregation_max: function(arr) {
+        return Math.max.apply(null, arr);
+    },
 });
